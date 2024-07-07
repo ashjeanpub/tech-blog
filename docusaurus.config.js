a0a2fcf93612018653d23678889e61abcc8d86e9
ashjean <ashjean.dev@gmail.com>
2024-07-07 14:34:06 +0900

    - 배포 테스트

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,6 +60,7 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
+          routeBasePath:'/',
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },

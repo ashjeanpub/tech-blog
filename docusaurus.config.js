7ac55ec75f8cd078cc1b27654456da5c846c5091
ashjean <ashjean.dev@gmail.com>
2024-07-07 14:08:10 +0900

    - 배포 테스트

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://ashjeanpub.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tech-blog/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

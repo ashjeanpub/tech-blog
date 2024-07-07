fed9d8d1ddf067f116f1b1a9b816640c7d2991a9
ashjean <ashjean.dev@gmail.com>
2024-07-07 20:47:38 +0900

    - 배포 테스트

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,8 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
             // Replace with your project's social card
-            image: 'img/docusaurus-social-card.jpg',
+            // 이미지 사용하지 않음
+            // image: 'img/docusaurus-social-card.jpg',
             navbar: {
                 // 상단 제목
                 title: 'My Site',

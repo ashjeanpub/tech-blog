3892c4a1c2cfed2d21507ddecc8377d126449d87
ashjean <ashjean.dev@gmail.com>
2024-07-07 17:47:34 +0900

    - 배포 테스트

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,10 +56,10 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           // 추가 설정
+          routeBasePath: '/', // Serve the docs at the site's root
         },
         // docs:false,
         blog: {
-          routeBasePath: '/', // Serve the docs at the site's root
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.

ea228f0fa7561c393e7e90edc9e83752e517d3f7
ashjean <ashjean.dev@gmail.com>
2024-07-07 15:26:56 +0900

    - 배포 테스트

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
           // routeBasePath: '/', // Serve the docs at the site's root
         },
         blog: {
-          routeBasePath: '/', // Serve the blog at the site's root
+          // routeBasePath: '/', // Serve the blog at the site's root
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.

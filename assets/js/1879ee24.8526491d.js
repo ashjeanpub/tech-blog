98448ee873713aac99e46a0650ea1014363c16dc
ashjeanpub <ashjean.pub@gmail.com>
2025-04-29 16:58:38 +0000

    Deploy website - based on cf4dc34f564af3146f4204e767b35bc663ccfbb6

--- assets/js/1879ee24.8526491d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktech_blog=self.webpackChunktech_blog||[]).push([[4680],{2273:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/tech-blog/","page":1,"postsPerPage":10,"totalPages":1,"totalCount":7,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ en/assets/js/4a39c493.5ac4a9a7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktech_blog=self.webpackChunktech_blog||[]).push([[5807],{6477:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/tech-blog/en/","page":1,"postsPerPage":10,"totalPages":1,"totalCount":8,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
